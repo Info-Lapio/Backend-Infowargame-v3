f5e49aeed046cf9c969b6df39ce46094830643e8
서건희 <seogunhee4@gmail.com>
2021-09-30 19:51:52 +0900

    [FIX]controller

--- src/main/java/info/wargame/backendinfowargamev3/controller/AuthController.java
+++ src/main/java/info/wargame/backendinfowargamev3/controller/AuthController.java
@@ -1,10 +1,32 @@
 package info.wargame.backendinfowargamev3.controller;
 
+import info.wargame.backendinfowargamev3.payload.request.SignInRequest;
+import info.wargame.backendinfowargamev3.payload.response.TokenResponse;
+import info.wargame.backendinfowargamev3.service.auth.AuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
 @RestController
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthService authService;
+
+    @PostMapping
+    public TokenResponse signIn(@RequestBody SignInRequest signInRequest) {
+        return authService.signIn(signInRequest);
+    }
+
+    @PostMapping("/admin")
+    public TokenResponse signInAdmin(@RequestBody SignInRequest signInRequest) {
+        return authService.signInAdmin(signInRequest);
+    }
+
+    @PutMapping
+    public TokenResponse refreshToken(@RequestHeader("X-Refresh-Token") String refreshToken,
+                                      @RequestHeader("Authorization") String accessToken) {
+        return authService.refreshToken(refreshToken, accessToken);
+    }
 }
